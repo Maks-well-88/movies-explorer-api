12cc40fab9f2c766ce4aedaa398596cc105636d4
Maksim Pisartsev <Maxwell88@yandex.ru>
2023-01-22 11:09:37 +0300

    Feat: add logging

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
