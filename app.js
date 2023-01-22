6612507ffab141caedae07c4117899af9c35e041
Maksim Pisartsev <Maxwell88@yandex.ru>
2023-01-22 16:35:51 +0300

    Feat: add db location to process.env

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { DB_LOCATION, PORT = 3000 } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -27,7 +27,7 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', (err) => {
+mongoose.connect(DB_LOCATION, (err) => {
   if (err) throw err;
   console.log('Connected to MongoDB');
 });
