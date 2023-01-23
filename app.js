8ef8dab530f39553610b81ce70e1aa8be370c3bf
Maksim Pisartsev <Maxwell88@yandex.ru>
2023-01-23 16:21:04 +0300

    Refactor: configure the rate limiter in a separate file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+const limiter = require('./utils/limiter');
 const errorHandler = require('./middlewares/error');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -12,13 +12,6 @@ const { DB_SRC_PUBLIC } = require('./utils/config');
 const app = express();
 const { NODE_ENV, DB_LOCATION, PORT = 3000 } = process.env;
 const dbLocation = NODE_ENV === 'production' ? DB_LOCATION : DB_SRC_PUBLIC;
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
 
 app.use(express.json());
 app.use(helmet());
